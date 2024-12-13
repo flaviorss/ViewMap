68f9c66e56d7678ef8830a9e0a6238e6a350d2a8
Flavio Santos <flawyo2002@gmail.com>
2024-12-13 18:45:32 -0300

    feat: adição dos algoritmos de clipping de reta

--- Trabalho II/Clipping.py
+++ Trabalho II/Clipping.py
@@ -11,14 +11,6 @@ class Pos(Enum):
     DENTRO = 1
     EM_CIMA = 2
 
-# Variaveis Globais
-INSIDE = 0  # Dentro da janela
-LEFT = 1  # Para a esquerda da janela
-RIGHT = 2  # Para a direita da janela
-BOTTOM = 4  # Abaixo da janela
-TOP = 8  # Acima da janela
-
-
 class ClippingPonto():
     def ponto_contido_recorte(ponto: Ponto, window: Recorte):
         if window.min.x <= ponto.x <= window.max.x and window.min.y <= ponto.y <= window.max.y:
@@ -26,61 +18,110 @@ class ClippingPonto():
         else:
             ponto.visivel = False
 
-
 class CohenSutherland():
 
     def define_codigo(ponto: Ponto, window: Recorte) -> int:
-        codigo = INSIDE
-        if ponto.x < window.min.x:
-            codigo += LEFT
-        elif ponto.x > window.max.x:
-            codigo += RIGHT
-        if ponto.y < window.min.y:
-            codigo += BOTTOM
-        elif ponto.y > window.max.y:
-            codigo += TOP
+        codigo = 0
+        if ponto.x > window.max.x:
+            codigo += 2
+        elif ponto.x < window.min.x:
+            codigo += 1
+
+        if ponto.y > window.max.y:
+            codigo = 8
+        elif ponto.y < window.min.y:
+            codigo = 4
 
         return codigo
 
-    def clipping_reta(self, reta: Segmento, window: Recorte):
-        c1 = CohenSutherland.define_codigo(reta.p1, window)
-        c2 = CohenSutherland.define_codigo(reta.p2, window)
+    def clipping_reta(reta: Segmento, window: Recorte):
+        done = False
+        codOut = 0
+        p = Ponto(0,0)
+        codA = CohenSutherland.define_codigo(reta.p1, window)
+        codB = CohenSutherland.define_codigo(reta.p2, window)
 
-        while True:
-            if c1 == INSIDE and c2 == INSIDE:
-                reta.visivel = True
-                return Segmento(reta.p1, reta.p2)
-            elif c1 != 0 and c2 != 0:
-                reta.visivel = False
-                return None
+        while not done:
+            if(codA | codB) == 0:
+                reta.visivel = done  = True
+            elif (codA & codB) != 0:
+                done = True
             else:
-                if c1 != INSIDE:
-                    outCodigo = c1
-                    outPonto = reta.p1
-                else:
-                    outCodigo = c2
-                    outPonto = reta.p2
+                if (codA != 0): codOut = codA
+                else: codOut = codB
 
-                x, y = outPonto.x, outPonto.y
-                # Calculo da interseção com a janela
-                if outCodigo & TOP:
-                    x = reta.p1.x + (reta.p2.x - reta.p1.x) * (window.max.y - reta.p1.y) / (reta.p2.y - reta.p1.y)
-                    y = window.max.y
-                elif outCodigo & BOTTOM:
-                    x = reta.p1.x + (reta.p2.x - reta.p1.x) * (window.min.y - reta.p1.y) / (reta.p2.y - reta.p1.y)
-                    y = window.min.y
-                elif outCodigo & LEFT:
-                    y = reta.p1.y + (reta.p2.y - reta.p1.y) * (window.min.x - reta.p1.x) / (reta.p2.x - reta.p1.x)
-                    x = window.min.x
-                elif outCodigo & RIGHT:
-                    y = reta.p1.y + (reta.p2.y - reta.p1.y) * (window.max.x - reta.p1.x) / (reta.p2.x - reta.p1.x)
-
-                if outCodigo == c1:
-                    reta.p1 = Ponto(x, y)
-                    c1 = CohenSutherland.define_codigo(reta.p1, window)
+                if reta.p1.x == reta.p2.x:
+                    if codOut & 0x08:
+                        p.y = window.max.y
+                    else:
+                        p.y = window.min.y
+                    p.x = reta.p1.x
+                elif reta.p1.y == reta.p2.y:
+                    if codOut & 0x01:
+                        p.x = window.min.x
+                    else:
+                        p.x = window.max.x
+                    p.y = reta.p1.y
                 else:
-                    reta.p2 = Ponto(x, y)
-                    c2 = CohenSutherland.define_codigo(reta.p2, window)
+                    m = (reta.p2.y - reta.p1.y) / (reta.p2.x - reta.p1.x)
+                    if codOut & 0x08:
+                        p.y = window.max.y
+                        p.x = reta.p1.x + (window.max.y - reta.p1.y)/m
+                    elif codOut & 0x04:
+                        p.y = window.min.y
+                        p.x = reta.p1.x + (window.min.y - reta.p1.y)/m
+                    elif codOut & 0x02:
+                        p.x = window.max.x
+                        p.y = reta.p1.y + m*(window.max.x - reta.p1.x)
+                    elif codOut & 0x01:
+                        p.x = window.min.x
+                        p.y = reta.p1.y + m*(window.min.x - reta.p1.x)
+
+                if codOut == codA:
+                    reta.p1 = Ponto(p.x, p.y)
+                    codA = CohenSutherland.define_codigo(reta.p1, window)
+                else:
+                    reta.p2 = Ponto(p.x, p.y)
+                    codB = CohenSutherland.define_codigo(reta.p2, window)
+
+
+class LiangBarsky():
+
+    def clipping_reta(reta: Segmento, window: Recorte):
+        reta.visivel = True
+        p = [0] * 4
+        q = [0] * 4
+        r = [0] * 4
+        u1 = 0
+        u2 = 1
+        deltaX = reta.p2.x - reta.p1.x
+        deltaY = reta.p2.y - reta.p1.y
+
+        p[0] = -deltaX
+        p[1] = deltaX
+        p[2] = -deltaY
+        p[3] = deltaY
+
+        q[0] = reta.p1.x - window.min.x
+        q[1] = window.max.x - reta.p1.x
+        q[2] = reta.p1.y - window.min.y
+        q[3] = window.max.y - reta.p1.y
+
+        for i in range(4):
+            if p[i] != 0:
+                r[i] = q[i] / p[i]
+                if p[i] < 0 and r[i] > u1:
+                    u1 = r[i]
+                elif p[i] > 0 and r[i] < u2:
+                    u2 = r[i]
+            elif q[i] < 0:
+                reta.visivel = False
+
+        if reta.visivel and u1 < u2:
+            Q1 = Ponto(reta.p1.x + deltaX * u1, reta.p1.y + deltaY * u1)
+            Q2 = Ponto(reta.p1.x + deltaX * u2, reta.p1.y + deltaY * u2)
+            reta.p1 = Q1
+            reta.p2 = Q2
 
 
 class WeilerAtherton():
